2fef74e5d681057e3b95fd77a069c9639b2ace54
poornimanagQB <poornima_ponthagani@mckinsey.com>
2025-09-11 12:44:24 +0100

    fix: Fix Namespace and path (#100)
    Bug Fix:
    - Fixed webhook service namespapce to ark-system
    - Fixed wheel file path matching with the path in Docker file 
    ## Checklist
    
    - [ ] Follow the [contributor guide](./CONTRIBUTING.md)
    - [ ] End-to-end tests pass
    - [ ] Unit tests for essential parts of your code, e.g. APIs exposed via
    SDKs or endpoints
    - [ ] Update `./docs`
    - [ ] Recognize contributors with "@all-contributors please add <person>
    for <code, bug, docs, etc>"
    - [ ] Linked issues #eg101

--- services/ark-api/ark-api/scripts/ensure-ark-sdk-wheel.sh
+++ services/ark-api/ark-api/scripts/ensure-ark-sdk-wheel.sh
@@ -8,11 +8,11 @@ if [ "$(git rev-parse --show-toplevel)" != "$(pwd)" ] || ! git remote get-url or
     exit 1
 fi
 
-if [ ! -f services/ark-api/ark-api/out/ark_sdk-*.whl ]; then
+if [ ! -f services/ark-api/out/ark_sdk-*.whl ]; then
     echo "Building ARK SDK wheel..."
     make ark-sdk-build
-    mkdir -p services/ark-api/ark-api/out
-    cp out/ark-sdk/py-sdk/dist/ark_sdk-*.whl services/ark-api/ark-api/out/
+    mkdir -p services/ark-api/out
+    cp out/ark-sdk/py-sdk/dist/ark_sdk-*.whl services/ark-api/out/
     echo "ARK SDK wheel ready"
 else
     echo "ARK SDK wheel exists"
